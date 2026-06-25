667bb9d49edcb21e5211934f376259e7b1fa384c
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-06-24 19:55:12 -0600

    Style: clean layout, separate fonts and define global style variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local'
+import { guidaMono, guidaMonoSharp } from "./fonts";
 import "./globals.css";
 
 
@@ -25,60 +25,6 @@ export const metadata: Metadata = {
         ],
     },
 };
-
-
-export const guidaMono = localFont({
-    src: [
-        {
-            path: '/fonts/GuidaMonoPro-Light.ttf',
-            weight: '300',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoPro-Regular.ttf',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoPro-Bold.ttf',
-            weight: '700',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoPro-Black.ttf',
-            weight: '900',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-guida-mono'
-});
-
-export const guidaMonoSharp = localFont({
-    src: [
-        {
-            path: '/fonts/GuidaMonoSharpPro-Light.ttf',
-            weight: '300',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoSharpPro-Regular.ttf',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoSharpPro-Bold.ttf',
-            weight: '700',
-            style: 'normal',
-        },
-        {
-            path: '/fonts/GuidaMonoSharpPro-Black.ttf',
-            weight: '900',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-guida-mono-sharp'
-});
-
 
 
 export default function RootLayout({
