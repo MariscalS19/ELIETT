9f8072df33a551ae67ca5999ad14e553cdb9c455
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-07-29 01:23:11 -0600

    refactor: reformat config and dependency files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["argon2-wasm"],
-  trailingSlash: true,
-  images: {
-    qualities: [100, 75],
-  },
+    serverExternalPackages: ['argon2-wasm'],
+    trailingSlash: true,
+    images: {
+        qualities: [100, 75],
+    },
 };
 
 export default nextConfig;
