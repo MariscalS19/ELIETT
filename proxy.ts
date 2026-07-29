035bc50ab69cada9fc7f13ef08bf311686ae82f7
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-07-29 01:26:23 -0600

    fix(middleware): fix backslash routes problems

--- proxy.ts
+++ proxy.ts
@@ -3,22 +3,33 @@ import { NextRequest } from 'next/server';
 import { verifyJWT } from './backend/utils/authUtils';
 
 export default async function proxy(request: NextRequest) {
-    const pathname = request.nextUrl.pathname.replace(/\/$/, '') || '/';
-    const token = request.cookies.get('admin_session')?.value;
+    const pathname = request.nextUrl.pathname;
 
-    const isValidToken = token ? await verifyJWT(token) : false;
+    if (pathname.startsWith('/login')) {
+        const token = request.cookies.get('admin_session')?.value;
+        const isValidToken = token ? await verifyJWT(token) : false;
+
+        if (isValidToken && request.method === 'GET') {
+            console.log(
+                'Redirecting to /admin/dashboard, user is already authenticated'
+            );
+            return NextResponse.redirect(
+                new URL('/admin/dashboard', request.url)
+            );
+        }
+
+        return NextResponse.next();
+    }
 
     if (pathname.startsWith('/admin')) {
-        if (!isValidToken) {
-            console.log('Redirecting to /login due to invalid or missing token');
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
-    };
+        const token = request.cookies.get('admin_session')?.value;
+        const isValidToken = token ? await verifyJWT(token) : false;
 
-    if (pathname.startsWith('/login') && request.method === 'GET') {
-        if (isValidToken) {
-            console.log('Redirecting to /admin/dashboard, user is already authenticated');
-            return NextResponse.redirect(new URL('/admin/dashboard', request.url));
+        if (!isValidToken) {
+            console.log(
+                'Redirecting to /login due to invalid or missing token'
+            );
+            return NextResponse.redirect(new URL('/login', request.url));
         }
     }
 
@@ -26,5 +37,5 @@ export default async function proxy(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/login', '/admin/:path*'],
+    matcher: ['/login', '/login/', '/admin/:path*'],
 };
