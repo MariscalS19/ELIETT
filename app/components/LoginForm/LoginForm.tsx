d051d3ef34014790324c7f68358d20be6ff023dd
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-08-11 21:45:46 -0600

    style: add react-icons, replace existing icons and change styles

--- app/components/LoginForm/LoginForm.tsx
+++ app/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 import styles from './LoginForm.module.css';
-import { PasswordIcon } from '../icons/PasswordIcon';
 import { useActionState, useEffect } from 'react';
+import { LuLock } from 'react-icons/lu';
 import { useRouter } from 'next/navigation';
 import { logAdminAction } from '@/backend/actions/authActions';
 
@@ -19,17 +19,38 @@ function LoginForm() {
     return (
         <div className={styles.loginContainer}>
             <div className={styles.loginHeader}>
-                <h1 className={styles.loginTitle}>Welcome <br /> back</h1>
+                <h1 className={styles.loginTitle}>
+                    Welcome <br /> Back
+                </h1>
                 <h2 className={styles.loginSubtitle}>Eliett</h2>
             </div>
             <form className={styles.loginForm} action={formAction}>
                 <div className={styles.inputWrapper}>
-                    <input id="password" name="password" type="password" placeholder="" className={`${styles.loginInput} ${hasError ? styles.inputError : ''}`} autoComplete="current-password" disabled={isPending} />
-                    <PasswordIcon className={`${styles.inputIcon} ${hasError ? styles.iconError : ''}`} />
-                    <label htmlFor="password" className={`${styles.loginLabel} ${hasError ? styles.labelError : ''}`}>Password</label>
-                    {hasError && <p className={styles.errorMessage}>{state.message}</p>}
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        placeholder=''
+                        className={`${styles.loginInput} ${hasError ? styles.inputError : ''}`}
+                        autoComplete='current-password'
+                        disabled={isPending}
+                    />
+                    <LuLock
+                        className={`${styles.inputIcon} ${hasError ? styles.iconError : ''}`}
+                    />
+                    <label
+                        htmlFor='password'
+                        className={`${styles.loginLabel} ${hasError ? styles.labelError : ''}`}>
+                        Password
+                    </label>
+                    {hasError && (
+                        <p className={styles.errorMessage}>{state.message}</p>
+                    )}
                 </div>
-                <button type="submit" className={styles.loginButton} disabled={isPending}>
+                <button
+                    type='submit'
+                    className={styles.loginButton}
+                    disabled={isPending}>
                     {isPending ? 'Verifying...' : 'Log In'}
                 </button>
             </form>
