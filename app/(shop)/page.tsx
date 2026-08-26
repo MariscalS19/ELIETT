8ff8b0c51ccaa057f674cdd03893dc2795bfb105
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-08-26 17:27:51 -0600

    refactor: add styles and not applied product preview logic

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,26 +1,58 @@
 import styles from './page.module.css';
 import Image from 'next/image';
-export default function Home() {
+import { getPublicProducts } from '@/backend/db/lib';
+
+export default async function Home() {
+    // const products = await getPublicProducts();
+
     return (
-        <main className={styles.main}>
-            <div className={styles.home_img_container}>
-                <Image
-                    className={styles.home_img}
-                    src='/mainCover.webp'
-                    alt='Background image'
-                    fill
-                    priority
-                    sizes='100vw'
-                    quality={100}
-                    unoptimized
-                />
-                <div className={styles.home_img_overlay}>
-                    <div className={styles.home_img_notice}>
-                        <p>We are working on it</p>
-                        <span>Come back soon !!!</span>
+        <div className={styles.page}>
+            <section className={styles.header}>
+                <div className={styles.header_img_container}>
+                    <Image
+                        className={styles.header_img}
+                        src='/mainCover.webp'
+                        alt='Background image'
+                        fill
+                        priority
+                        sizes='100vw'
+                        quality={100}
+                    />
+                    <div className={styles.header_img_overlay}>
+                        <div className={styles.header_img_notice}>
+                            <p>We are working on it</p>
+                            <span>Come back soon !!!</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </section>
+
+            {/* <section className={styles.home_grid}>
+                {products.map((product) => {
+                    const image =
+                        product.images.find((item) => item.position === 1)
+                            ?.image_url ||
+                        product.images[0]?.image_url ||
+                        '/mainCover.webp';
+
+                    return (
+                        <div className={styles.home_item} key={product.id}>
+                            <div className={styles.img_container}>
+                                <Image
+                                    className={styles.item_img}
+                                    src={image}
+                                    alt={`Image of ${product.name}`}
+                                    fill
+                                    unoptimized
+                                    sizes='100vw'
+                                    quality={85}
+                                />
+                            </div>
+                            <h3 className={styles.item_name}>{product.name}</h3>
+                        </div>
+                    );
+                })}
+            </section> */}
+        </div>
     );
 }
