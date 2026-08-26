4cd668ce3913e552f845cd4b11ae6c1fe611b679
Hector Mariscal <mariscalsanchez19@gmail.com>
2026-08-25 19:13:27 -0600

    add UI libraries and increase config body size

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     serverExternalPackages: ['argon2-wasm'],
     trailingSlash: true,
+    experimental: {
+        proxyClientMaxBodySize: '25mb',
+        serverActions: {
+            bodySizeLimit: '25mb',
+        },
+    },
     images: {
-        qualities: [100, 75],
+        qualities: [100, 85],
     },
 };
 
